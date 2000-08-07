433c119489a0886a8113978f3769866c3c4cbddc
neotron <neotron@d22b66b4-c94e-0410-8570-a1be9e963a58>
2000-08-07 23:03:31 +0000

    added performance.h and cleaned config.h

--- caudium/trunk/server/etc/include/config.h
+++ caudium/trunk/server/etc/include/config.h
@@ -2,10 +2,12 @@
  * $Id$
  *
  * User configurable things not accessible from the normal
- * configuration interface. Not much, but there are some things..  
+ * configuration interface. Not much, but there are some things..
+ * Also see performance.h for more configurable options.
  */
 
 #include <extra_config.h>
+#include <performance.h>
 #ifndef _ROXEN_CONFIG_H_
 #define _ROXEN_CONFIG_H_
 
@@ -21,16 +23,6 @@
 #endif /* ENABLE_THREADS */
 #endif /* !DISABLE_THREADS */
 #endif /* efun(thread_create) */
-
-
-/* Reply 'PONG\r\n' to the query 'PING\r\n'.
- * For performance tests...
- */
-#define SUPPORT_PING_METHOD
-#define SUPPORT_HTACCESS
-
-
-
 
 /* Lev  What                          Same as defining
  *----------------------------------------------------
@@ -78,83 +70,10 @@
 #define FD_DEBUG
 #endif
 
-/* Do we want module level deny/allow security (IP-numbers and usernames). 
- * 1% speed loss, as an average. (That is, if your CPU is used to the max.
- * it probably isn't..)  
- */
-#ifndef NO_MODULE_LEVEL_SECURITY
-# define MODULE_LEVEL_SECURITY
-#endif
-
-
-/* If this is disabled, the server won't parse the supports string. This might
- * make the server somewhat faster. If you don't need this feature but need the
- * most speed you can get, it might be a good idea to disable supports.
- */
-
-// #define DISABLE_SUPPORTS
-
-
-/* Roxen neighbourhood
- *
- * Experimental. Currently does not work on all Operating Systems.
- * *** WARNING *** There are reports of this not working at all. It's
- * not supported, not maintained and CAN LOCK UP YOUR SERVER!
- */
-// #define ENABLE_NEIGHBOURHOOD
-
-/* If set, the maximum, minimum and average time used to serve
- * requests is logged.
- * This (rusage()) is broken on some systems, and the server will be
- * somewhat ( < 5% ) slower with this enabled.  
- *
- * CURRENTLY NOT SUPPORTED, WORK IN PROGRESS, It _did_ work in 1.0b4 :-)
- */
-#undef USE_RUSAGE
-
-
-/* Define this if you don't want Roxen to use DNS. Note: This
- * doesn't make the server itself faster. It only reduces the netload
- * some. This option turns off ALL ip -> hostname and hostname -> ip
- * conversion. Thus you can't use if if you want to run a proxy. 
- */
-
-#undef NO_DNS
-
-
-/* This option turns of all ip->hostname lookups. However the
- * hostname->ip lookups are still functional. This _is_ usable
- * if you run a proxy.. :-)
- */
-
-#undef NO_REVERSE_LOOKUP
-
-
 /* Should we use sete?id instead of set?id?.
  * There _might_ be security problems with the sete?id functions.
  */
 
 #define SET_EFFECTIVE 
 
-
-/*---------------- End of configurable options. */
-
 #endif /* if _ROXEN_CONFIG_H_ */
-
-/* Should we be compatible with level b9 and below configuration files? */
-#undef COMPAT
-
-/*
- * Should support for URL modules be included?
- * I am trying to phase them out, but..
- */
-#define URL_MODULES
-
-/* Basically, should it be o.k. to return "string" as a member of
- * the result mapping? This is only for compability.
- * Normally: ([ "data":long_string, "type":"text/html" ]), was
- * ([ "string":long_string, "type":"text/html" ]), please ignore..
- * Do not use this, unless you _really_ want to make your
- * modules unportable :-)
- */
-#undef API_COMPAT
