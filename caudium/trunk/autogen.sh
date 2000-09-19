16a10f729dcce3fea7005b146a745c5576ece77c
neotron <neotron@d22b66b4-c94e-0410-8570-a1be9e963a58>
2000-09-19 18:28:30 +0000

    more quiet?

--- caudium/trunk/autogen.sh
+++ caudium/trunk/autogen.sh
@@ -4,9 +4,9 @@ mydir=`pwd`
 for a in `find src tools -name "autogen.sh" |sort`; do
   dir=`dirname $a`
   echo "Running autogen in '$dir'"
-  cd $dir
+  cd $dir >/dev/null 2>&1
   ./autogen.sh $mydir
-  cd $mydir
+  cd $mydir >/dev/null 2>&1
 done  
 
 autoconf >/dev/null 2>&1
