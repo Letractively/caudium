d8bf72b886843a135ae128d408961e9635015762
neotron <neotron@d22b66b4-c94e-0410-8570-a1be9e963a58>
2001-02-20 10:05:34 +0000

    Data sending kinda works now.

--- caudium/trunk/src/cmods/Caudium/caudium.c
+++ caudium/trunk/src/cmods/Caudium/caudium.c
@@ -696,7 +696,7 @@ void pike_module_init( void )
   set_init_callback(alloc_buf_struct);
   set_exit_callback(free_buf_struct);
   end_class("ParseHTTP", 0);
-  init_nb_send();
+  init_nbio();
 }
 
 /* Restore and exit module */
@@ -708,6 +708,7 @@ void pike_module_exit( void )
   free_string(STRS(protocol));
   free_string(STRS(query));
   free_string(STRS(raw_url));
+  exit_nbio();
 }
 
 /*
