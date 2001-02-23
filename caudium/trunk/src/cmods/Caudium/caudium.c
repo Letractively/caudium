112a14653953dc7c79121feb0bbc1de53d870553
neotron <neotron@d22b66b4-c94e-0410-8570-a1be9e963a58>
2001-02-23 16:53:59 +0000

    Indent..

--- caudium/trunk/src/cmods/Caudium/caudium.c
+++ caudium/trunk/src/cmods/Caudium/caudium.c
@@ -51,23 +51,23 @@ static INLINE unsigned char *char_decode_url(unsigned char *str, int len) {
   endl2 = end-2; /* to see if there's enough left to make a "hex" char */
   for (nlen = 0, i = 0; ptr < end; i++) {
     switch(*ptr) {
-        case '%':
-          if (ptr < endl2)
-            str[i] = (((ptr[1] < 'A') ? (ptr[1] & 15) :(( ptr[1] + 9) &15)) << 4)|
-              ((ptr[2] < 'A') ? (ptr[2] & 15) : ((ptr[2] + 9)& 15));
-          else
-            str[i] = '\0';
-          ptr+=3;
-          nlen++;
-          break;
-        case '?':
-          /* Don't decode more, reached query string*/
-          str[i] = '\0';
-          return (ptr+1);
-          break;
-        default:
-          str[i] = *(ptr++);
-          nlen++;
+     case '%':
+      if (ptr < endl2)
+	str[i] = (((ptr[1] < 'A') ? (ptr[1] & 15) :(( ptr[1] + 9) &15)) << 4)|
+	  ((ptr[2] < 'A') ? (ptr[2] & 15) : ((ptr[2] + 9)& 15));
+      else
+	str[i] = '\0';
+      ptr+=3;
+      nlen++;
+      break;
+     case '?':
+      /* Don't decode more, reached query string*/
+      str[i] = '\0';
+      return (ptr+1);
+      break;
+     default:
+      str[i] = *(ptr++);
+      nlen++;
     }
   }
   str[nlen] = '\0'; /* We will use make_shared_string since a file can't
@@ -227,26 +227,26 @@ static void f_buf_create( INT32 args )
   if(BUF->data != NULL)
     Pike_error("Create already called!\n");
   switch(args) {
-      default:
-        Pike_error("Wrong number of arguments to create. Expected 2 or 3.\n");
-        break;
+   default:
+    Pike_error("Wrong number of arguments to create. Expected 2 or 3.\n");
+    break;
 	  
-      case 3:
-        if(Pike_sp[-1].type != T_INT) {
-          Pike_error("Wrong argument 3 to create. Expected int.\n");
-        } else if(Pike_sp[-1].u.integer < 100) {
-          Pike_error("Specified buffer too small.\n");
-        } else {
-          BUF->free = Pike_sp[-1].u.integer;
-        }
-        /* fall through */
+   case 3:
+    if(Pike_sp[-1].type != T_INT) {
+      Pike_error("Wrong argument 3 to create. Expected int.\n");
+    } else if(Pike_sp[-1].u.integer < 100) {
+      Pike_error("Specified buffer too small.\n");
+    } else {
+      BUF->free = Pike_sp[-1].u.integer;
+    }
+    /* fall through */
 	  
-      case 2:
-        if(Pike_sp[-(args - 1)].type != T_MAPPING)
-          Pike_error("Wrong argument 2 to create. Expected mapping.\n");	
-        if(Pike_sp[-args].type != T_MAPPING)
-          Pike_error("Wrong argument 1 to create. Expected mapping.\n");
-        break;
+   case 2:
+    if(Pike_sp[-(args - 1)].type != T_MAPPING)
+      Pike_error("Wrong argument 2 to create. Expected mapping.\n");	
+    if(Pike_sp[-args].type != T_MAPPING)
+      Pike_error("Wrong argument 1 to create. Expected mapping.\n");
+    break;
   }
 
   if(BUF->free) {
@@ -361,24 +361,24 @@ static struct pike_string *url_decode(unsigned char *str, int len, int exist)
 
   for (i = exist; ptr < end; i++) {
     switch(*ptr) {
-        case '%':
-          if (ptr < endl2)
-            mystr[i] =
-              (((ptr[1] < 'A') ? (ptr[1] & 15) :(( ptr[1] + 9) &15)) << 4)|
-              ((ptr[2] < 'A') ? (ptr[2] & 15) : ((ptr[2] + 9)& 15));
-          else
-            mystr[i] = '\0';
-          ptr+=3;
-          nlen++;
-          break;
-        case '+':
-          ptr++;
-          mystr[i] = ' ';
-          nlen++;
-          break;
-        default:
-          mystr[i] = *(ptr++);
-          nlen++;
+     case '%':
+      if (ptr < endl2)
+	mystr[i] =
+	  (((ptr[1] < 'A') ? (ptr[1] & 15) :(( ptr[1] + 9) &15)) << 4)|
+	  ((ptr[2] < 'A') ? (ptr[2] & 15) : ((ptr[2] + 9)& 15));
+      else
+	mystr[i] = '\0';
+      ptr+=3;
+      nlen++;
+      break;
+     case '+':
+      ptr++;
+      mystr[i] = ' ';
+      nlen++;
+      break;
+     default:
+      mystr[i] = *(ptr++);
+      nlen++;
     }
   }
 
@@ -413,26 +413,26 @@ INLINE static int get_next_header(unsigned char *heads, int len,
   
   for(count=0, colon=0; count < len; count++) {
     switch(heads[count]) {
-        case ':':
-          colon = count;
-          data = colon + 1;
-          for(count2 = data; count2 < len; count2++)
-            /* find end of header data */
-            if(heads[count2] == '\r') break;
-          while(heads[data] == ' ') data++;
+     case ':':
+      colon = count;
+      data = colon + 1;
+      for(count2 = data; count2 < len; count2++)
+	/* find end of header data */
+	if(heads[count2] == '\r') break;
+      while(heads[data] == ' ') data++;
       
-          skey.u.string = lowercase(heads, colon);      
-          if (skey.u.string == NULL) return -1;
-          sval.u.string = make_shared_binary_string((char *)(heads+data),
-                                                    count2 - data);
-          mapping_insert(headermap, &skey, &sval);
-          count = count2;
-          free_string(skey.u.string);
-          free_string(sval.u.string);
-          break;
-        case '\n':
-          /*printf("Returning %d read\n", count);*/
-          return count+1;
+      skey.u.string = lowercase(heads, colon);      
+      if (skey.u.string == NULL) return -1;
+      sval.u.string = make_shared_binary_string((char *)(heads+data),
+						count2 - data);
+      mapping_insert(headermap, &skey, &sval);
+      count = count2;
+      free_string(skey.u.string);
+      free_string(sval.u.string);
+      break;
+     case '\n':
+      /*printf("Returning %d read\n", count);*/
+      return count+1;
     }
   }
   return count;
@@ -489,52 +489,52 @@ static void f_parse_query_string( INT32 args )
   for(; ptr <= end; ptr++) {
     switch(*ptr)
     {
-        case '=':
-          /* Allow an unencoded '=' in the value. It's invalid but... */
-          if(equal == NULL)
-            equal=ptr;
-          break;
-        case '\0':
-          if(ptr != end)
-            continue;
-        case ';': /* It's recommended to support ';'
-                     instead of '&' in query strings... */
-        case '&':
-          if(equal == NULL) { /* value less variable, we can ignore this */
-            name = ptr+1;
-            break;
-          }
-          namelen = equal - name;
-          valulen = ptr - ++equal;
-          skey.u.string = url_decode(name, namelen, 0);
-          if (skey.u.string == NULL) { /* OOM. Bail out */
-            Pike_error("Caudium.parse_query_string(): Out of memory in url_decode().\n");
-          }
-          exist = low_mapping_lookup(variables, &skey);
-          if(exist == NULL || exist->type != T_STRING) {
-            sval.u.string = url_decode(equal, valulen, 0);
-            if (sval.u.string == NULL) { /* OOM. Bail out */
-              Pike_error("Caudium.parse_query_string(): "
-                         "Out of memory in url_decode().\n");
-            }
-          } else {
-            /* Add strings separed with '\0'... */
-            struct pike_string *tmp;
-            tmp = url_decode(equal, valulen, 1);
-            if (tmp == NULL) {
-              Pike_error("Caudium.parse_query_string(): "
-                         "Out of memory in url_decode().\n");
-            }
-            sval.u.string = add_shared_strings(exist->u.string, tmp);
-            free_string(tmp);
-          }
-          mapping_insert(variables, &skey, &sval);
-          free_string(skey.u.string);
-          free_string(sval.u.string);
+     case '=':
+      /* Allow an unencoded '=' in the value. It's invalid but... */
+      if(equal == NULL)
+	equal=ptr;
+      break;
+     case '\0':
+      if(ptr != end)
+	continue;
+     case ';': /* It's recommended to support ';'
+		  instead of '&' in query strings... */
+     case '&':
+      if(equal == NULL) { /* value less variable, we can ignore this */
+	name = ptr+1;
+	break;
+      }
+      namelen = equal - name;
+      valulen = ptr - ++equal;
+      skey.u.string = url_decode(name, namelen, 0);
+      if (skey.u.string == NULL) { /* OOM. Bail out */
+	Pike_error("Caudium.parse_query_string(): Out of memory in url_decode().\n");
+      }
+      exist = low_mapping_lookup(variables, &skey);
+      if(exist == NULL || exist->type != T_STRING) {
+	sval.u.string = url_decode(equal, valulen, 0);
+	if (sval.u.string == NULL) { /* OOM. Bail out */
+	  Pike_error("Caudium.parse_query_string(): "
+		     "Out of memory in url_decode().\n");
+	}
+      } else {
+	/* Add strings separed with '\0'... */
+	struct pike_string *tmp;
+	tmp = url_decode(equal, valulen, 1);
+	if (tmp == NULL) {
+	  Pike_error("Caudium.parse_query_string(): "
+		     "Out of memory in url_decode().\n");
+	}
+	sval.u.string = add_shared_strings(exist->u.string, tmp);
+	free_string(tmp);
+      }
+      mapping_insert(variables, &skey, &sval);
+      free_string(skey.u.string);
+      free_string(sval.u.string);
 
-          /* Reset pointers */
-          equal = NULL;
-          name = ptr+1;
+      /* Reset pointers */
+      equal = NULL;
+      name = ptr+1;
     }
   }
   pop_n_elems(args);
@@ -590,29 +590,29 @@ static void f_parse_prestates( INT32 args )
       int len = i - last_start;
       
       switch(done_first) {
-          case 0:
-            if (!MEMCMP(&url->str[last_start], "internal", len)) {
-              done_first = -1;
-              ind.u.string = make_shared_string("internal");
-            } else {
-              done_first = 1;
-              ind.u.string = make_shared_binary_string(&url->str[last_start], len);
-            }
+       case 0:
+	if (!MEMCMP(&url->str[last_start], "internal", len)) {
+	  done_first = -1;
+	  ind.u.string = make_shared_string("internal");
+	} else {
+	  done_first = 1;
+	  ind.u.string = make_shared_binary_string(&url->str[last_start], len);
+	}
             
-            multiset_insert(prestate, &ind);
-            break;
+	multiset_insert(prestate, &ind);
+	break;
             
-          case -1:
-            /* internal */
-            ind.u.string = make_shared_binary_string(&url->str[last_start], len);
-            multiset_insert(internal, &ind);
-            break;
+       case -1:
+	/* internal */
+	ind.u.string = make_shared_binary_string(&url->str[last_start], len);
+	multiset_insert(internal, &ind);
+	break;
             
-          default:
-            /* prestate */
-            ind.u.string = make_shared_binary_string(&url->str[last_start], len);
-            multiset_insert(prestate, &ind);
-            break;
+       default:
+	/* prestate */
+	ind.u.string = make_shared_binary_string(&url->str[last_start], len);
+	multiset_insert(prestate, &ind);
+	break;
       }
       last_start = i + 1;
     }
