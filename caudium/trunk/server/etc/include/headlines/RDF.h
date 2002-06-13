15408402d9d1a86fd394cb8ed24c0c48e880f617
grendel <grendel@d22b66b4-c94e-0410-8570-a1be9e963a58>
2002-06-13 01:50:46 +0000

    more improvements

--- caudium/trunk/server/etc/include/headlines/RDF.h
+++ caudium/trunk/server/etc/include/headlines/RDF.h
@@ -5,21 +5,49 @@
  *
  */
 
-static private string parse_it(string tag, mapping args, string|int contents,
+static private string ignore_it(string tag, mapping args, string|int contents,
 			       mapping hl)
 {
+    return "";
+}
+
+static private string parse_channel(string tag, mapping args, string|int contents,
+			       mapping hl)
+{
+    report_notice("parse_channel(%O)\n", tag);
+    
+    switch (tag) {
+	case "channel":
+	    parse_html(contents, ([]), ([ "link" : parse_channel, "description" : parse_channel,
+	                                  "pubDate" : parse_channel, "item" : ignore_it  ]), hl);
+	    break;
+	    
+	default:
+	    hl["ch_" + tag] = trim(contents);
+	    break;
+    }
+    
+    return "";
+}
+
+static private string parse_it(string tag, mapping args, string|int contents,
+			       mapping hl, mapping|void chhl)
+{
+  report_notice("parse_it(%O)\n", tag);
   mixed tmp;
   switch(tag)
-  {
+  {    
   case "item":
     hl = ([]);
     parse_html(contents, ([ ]), ([ "title": parse_it, "link": parse_it, "description" : parse_it  ]),
-	       hl);
+	       hl, chhl);
+    if (chhl)
+	hl += chhl;
     headlines += ({ hl });
     break;
 
   case "link":
-    tag = "url";
+   tag = "url";
   default:
     hl[tag] = trim(contents);
     break;
@@ -29,7 +57,10 @@ static private string parse_it(string tag, mapping args, string|int contents,
 
 private static void rdf_parse_reply(string data)
 {
-  parse_html(data, ([]), (["item" : parse_it ]) );
+  mapping chhl = ([]);
+  
+  parse_html(data + " ", ([]), (["channel" : parse_channel ]), chhl );
+  parse_html(data + " ", ([]), (["item" : parse_it ]), 0, chhl );
 }
 
   
