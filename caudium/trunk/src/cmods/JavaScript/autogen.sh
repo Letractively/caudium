03d3c8f136645276bcd66596b3656ab488e18a68
kiwi <kiwi@d22b66b4-c94e-0410-8570-a1be9e963a58>
2002-06-19 17:46:05 +0000

    Fix Fix Fix !

--- caudium/trunk/src/cmods/JavaScript/autogen.sh
+++ caudium/trunk/src/cmods/JavaScript/autogen.sh
@@ -3,9 +3,9 @@
 # FreeBSD specify hack
 if [ -x /usr/local/bin/autoconf213 ];
 then
-  autoconf213 --localdir=$1
   autoheader213
+  autoconf213 --localdir=$1
 else
-  autoconf --localdir=$1
   autoheader
+  autoconf --localdir=$1
 fi
