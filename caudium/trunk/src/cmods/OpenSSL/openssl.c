e56045002096d2fc0f427af70f04ab58e06e5927
kiwi <kiwi@d22b66b4-c94e-0410-8570-a1be9e963a58>
2002-09-17 15:08:22 +0000

    Added blurbs

--- caudium/trunk/src/cmods/OpenSSL/openssl.c
+++ caudium/trunk/src/cmods/OpenSSL/openssl.c
@@ -1,6 +1,28 @@
 /*
-**  openssl.c
-*/
+ * Pike Extension Modules - A collection of modules for the Pike Language
+ * Copyright © 2002 The Caudium Group
+ * Copyright © 2001 Paul Wankadia <junyer@gmx.net>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+/*
+ * $Id$
+ */
+
+
 
 #include "config.h"
 #include "global.h"
