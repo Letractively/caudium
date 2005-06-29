5e9c9b910c882366d1bc94bb633bd973adb68b2f
nobody <nobody@d22b66b4-c94e-0410-8570-a1be9e963a58>
2005-06-29 14:00:26 +0000

    This commit was manufactured by cvs2svn to create tag 'caudium_1_4_7'.

---
+++ caudium/tags/caudium_1_4_7/src/java/src/net/caudium/caudium/Frame.java
@@ -0,0 +1,21 @@
+/*
+ * $Id$
+ *
+ */
+
+package net.caudium.caudium;
+
+/**
+ * An object representing an RXML parse frame
+ *
+ * @see SimpleTagCaller
+ *
+ * @version	$Version$
+ * @author	marcus
+ */
+
+public final class Frame {
+
+  Frame() { }
+
+}
